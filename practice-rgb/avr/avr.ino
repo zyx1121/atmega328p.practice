2533485e93183a545b65c489a95f1a015a618fde
Loki <98001197+zyx1121@users.noreply.github.com>
2023-06-26 21:10:34 +0800

    updata

---
+++ practice-rgb/avr/avr.ino
@@ -0,0 +1,34 @@
+//
+//  filename    : avr.ino
+//  description : RGB and VR control
+//  author      : Loki
+//
+
+#include "rgb.h"
+#include "vr.h"
+
+//
+//  setup
+//
+void setup(void) {
+  InitRGB();
+  InitVR();
+}
+
+//
+//  loop
+//
+void loop(void) {
+  LoopVR();
+
+  for (uint8_t i = 0; i < 8; i++)
+    rgb[i][0] = vr,
+    rgb[i][1] = 0,
+    rgb[i][2] = 255 - vr;
+
+  LoopRGB();
+
+  FastLED.show();
+
+  delay(1);
+}
